f1693125e66c6e550fd92994e48c2057aa0ebe51
Matthes-Baer <Matthes.baer@yahoo.de>
2023-08-16 16:03:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Model } from "./BlenderFile";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas camera={{ fov: 64, position: [-2, 2, 0] }}>
+        <ambientLight intensity={5} />
+        <OrbitControls enableZoom={true} />
+        <Model />
+      </Canvas>
     </div>
   );
 }
